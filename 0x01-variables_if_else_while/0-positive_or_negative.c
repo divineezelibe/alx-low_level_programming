6d1d18bc740489c3da230e8edb1ba36ddc50fa26
Divine Ezelibe <divineezelibe.e@gmail.com>
2022-12-09 05:59:38 -0800

    Remove whitespaces in task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -4,8 +4,8 @@
 
 /**
  * main - Prints a random number and states whether
- * it is positive, negative, or zero. 
- * Return: Always 0.    
+ * it is positive, negative, or zero.
+ * Return: Always 0.
  */
 int main(void)
 {
