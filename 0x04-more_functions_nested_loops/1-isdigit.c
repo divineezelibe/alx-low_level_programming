dd07f00e4c90c145519a2b7ccaf06ac0fd9b94cd
Divine Ezelibe <divineezelibe.e@gmail.com>
2022-12-16 02:13:12 +0000

    Updated

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _isupper - Checks for uppercase characters
+ * _isdigit - Checks for uppercase characters
  * @c: Integer parameter
  *
  * Return: 1
@@ -9,7 +9,7 @@
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= '0' && c <= '9')
 		return (1);
 	else
 		return (0);
