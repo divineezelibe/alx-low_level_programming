f00704d2d55c29b78f6d08388b7859f92678877d
Divine Ezelibe <divineezelibe.e@gmail.com>
2022-12-16 02:59:17 +0000

    Updated Task

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,6 +1,3 @@
-#ifndef MAIN_H
-#define MAIN_H
-
 /**
  * Author: Divine Ezelibe
  * Description: All function prototypes are here
@@ -11,5 +8,3 @@ int _isdigit(int c);
 int mul(int a, int b);
 void print_numbers(void);
 void print_most_numbers(void);
-
-#endif
