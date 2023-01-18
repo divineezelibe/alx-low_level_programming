3f34af37b3ff3c5cf668df41f6984e3d149bc302
Divine Ezelibe <divineezelibe.e@gmail.com>
2023-01-18 02:56:07 +0000

    Update in task 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,63 +1,55 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "dog.h"
-#include <string.h>
+
+int get_len(int i);
+char *str_cpy(char *dest, char *src);
 
 /**
- * _strdup - returns a pointer to a newly allocated space in memory
- * contains a copy of the string given as a parameter.
- * @str: input array
- * Return: pointer to array
- */
-
-char *_strdup(char *str)
-{
-	char *p;
-	int i, size = 0;
-
-	if (!str)
-		return (0);
-	while (str[size] != '\0')
-	{
-		size++;
-	}
-	p = malloc((size + 1) * sizeof(char));
-	if (p == NULL)
-		return (p);
-	for (i = 0; i < size; i++)
-	{
-		p[i] = str[i];
-	}
-	p[i] = '\0';
-	return (p);
-}
-
-/**
- * new_dog - check the code for Holberton School students.
- * @name: name's dog
- * @age: age's dog
- * @owner: owner's dog
- * Return: new dog pointer
+ * new_dog - a function that creates a new dog
+ * get len of name + owner, malloc them, cpy name + owner to new
+ * @name: name
+ * @age: age
+ * @owner: owner
+ * Return: 0
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *new;
+	dog_t *new_name;
+	char *copy_name, *copy_owner;
+	unsigned int x, name_len = 0, owner_len = 0;
 
-	new = malloc(sizeof(dog_t));
-	if (new == NULL)
+	new_name = malloc(sizeof(dog_t));
+	if (name == NULL)
 		return (NULL);
-	new->name = _strdup(name);
-	if (new->name == 0)
-		free(new);
-	new->age = age;
-	new->owner = _strdup(owner);
-	if (new->owner == 0)
+	if (name == NULL || age <= 0 || owner == NULL)
 	{
-		if (new->name != 0)
-			free(new->name);
-			free(new);
+		free(new_name);
+		return (NULL);
 	}
 
-	return (new);
+	for (x = 0; name[x] != '\0'; x++)
+		name_len++;
+
+	for (x = 0; owner[x] != '\0'; x++)
+		owner_len++;
+
+	copy_name = malloc(sizeof(char) * (name_len + 1));
+	if (copy_name == NULL)
+		return (NULL);
+	copy_owner = malloc(sizeof(char) * (owner_len + 1));
+	if (copy_owner == NULL)
+		return (NULL);
+
+	for (x = 0; x <= name_len; x++)
+		copy_name[x] = name[x];
+
+	for (x = 0; x <= owner_len; x++)
+		copy_owner[x] = owner[x];
+	new_name->name = copy_name;
+	new_name->owner = copy_owner;
+	new_name->age = age;
+
+	return (new_name);
 }
