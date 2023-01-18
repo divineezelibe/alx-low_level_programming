fb29765cfca00ffdd25d02dc0ca8498f6dfdbbc1
Divine Ezelibe <divineezelibe.e@gmail.com>
2023-01-18 02:50:31 +0000

    Write a function that creates a new dog.
    Prototype: dog_t *new_dog(char *name, float age, char *owner);
    You have to store a copy of name and owner
    Return NULL if the function fails

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -1,39 +1,63 @@
-#include "dog.h"
+#include <stdio.h>
 #include <stdlib.h>
+#include "dog.h"
+#include <string.h>
 
 /**
- * new_dog - creates a new struct of type dog
- * @name: struct parameter name
- * @age: struct parameter age
- * @owner: struct parameter owner
- * Return: returns pointer to buffer of datatype dog
+ * _strdup - returns a pointer to a newly allocated space in memory
+ * contains a copy of the string given as a parameter.
+ * @str: input array
+ * Return: pointer to array
+ */
+
+char *_strdup(char *str)
+{
+	char *p;
+	int i, size = 0;
+
+	if (!str)
+		return (0);
+	while (str[size] != '\0')
+	{
+		size++;
+	}
+	p = malloc((size + 1) * sizeof(char));
+	if (p == NULL)
+		return (p);
+	for (i = 0; i < size; i++)
+	{
+		p[i] = str[i];
+	}
+	p[i] = '\0';
+	return (p);
+}
+
+/**
+ * new_dog - check the code for Holberton School students.
+ * @name: name's dog
+ * @age: age's dog
+ * @owner: owner's dog
+ * Return: new dog pointer
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	int nlen, olen, i;
+	dog_t *new;
 
-	dog_t *doggy;
-	nlen = olen = 0;
-	while (name[nlen++])
-		;
-	while (owner[olen++])
-		;
-	doggy = malloc(sizeof(dog_t));
-	if (doggy == NULL)
+	new = malloc(sizeof(dog_t));
+	if (new == NULL)
 		return (NULL);
+	new->name = _strdup(name);
+	if (new->name == 0)
+		free(new);
+	new->age = age;
+	new->owner = _strdup(owner);
+	if (new->owner == 0)
+	{
+		if (new->name != 0)
+			free(new->name);
+			free(new);
+	}
 
-	doggy->name = malloc(nlen * sizeof(doggy->name));
-	if (doggy == NULL)
-		return (NULL);
-	for (i = 0; i < nlen; i++)
-		doggy->name[i] = name[i];
-
-	doggy->age = age;
-	doggy->owner = malloc(olen * sizeof(doggy->owner));
-	if (doggy == NULL)
-		return (NULL);
-	for (i = 0; i < olen; i++)
-		doggy->owner[i] = owner[i];
-	return (doggy);
+	return (new);
 }
